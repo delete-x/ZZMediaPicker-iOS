ea960c4e4e9053f5d855e97d0921f12798ca6549
任强宾 <1850665560@qq.com>
2019-11-14 11:39:25 +0800

    no message

---
+++ Example/ZZMediaPickerDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ZZMediaPickerDemo
+//
+//  Created by 任强宾 on 2019/11/13.
+//  Copyright © 2019 renqiangbin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Example/ZZMediaPickerDemo/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  ZZMediaPickerDemo
+//
+//  Created by 任强宾 on 2019/11/13.
+//  Copyright © 2019 renqiangbin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
